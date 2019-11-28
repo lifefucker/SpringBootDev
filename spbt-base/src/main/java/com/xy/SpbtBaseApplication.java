f95237ab9a4093c168dca3733ca70f665d52fcc4
lifefucker <1373521920@qq.com>
2019-11-28 16:28:53 +0800

    init project

---
+++ spbt-base/src/main/java/com/xy/SpbtBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpbtBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpbtBaseApplication.class, args);
+	}
+
+}
